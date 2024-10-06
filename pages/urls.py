db1b74d104f546d406a6e6f632d303c996fe7848
NorIslam04 <islamaoudi18@gmail.com>
2024-10-06 21:22:11 +0100

    filters DTL in html file

--- pages/urls.py
+++ pages/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
 
-    path('index/', views.index, name='index'),
+    path('', views.index, name='index'),
     path('about/', views.about, name='about')
 
 ]
--- pages/views.py
+++ pages/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse as hr
 
 
 def index(req):
-    x={'name': 'islam', 'age' : 20}
+    x={'name': 'Islam Aoudia Nour', 'age' :""}
     return render(req,"pages/index.html",x)
 
 def about(req):
