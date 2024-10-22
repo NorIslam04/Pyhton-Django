f33c6f70b51e4bd34fda109650e772b03f0b6321
NorIslam04 <islamaoudi18@gmail.com>
2024-10-22 01:28:31 +0100

    collect ing and not file css

--- project/settings.py
+++ project/settings.py
@@ -117,7 +117,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR,"static") #DISTINATION
+
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,"project/static")
+] #SOURCE
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
