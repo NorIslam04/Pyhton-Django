990388ec6a1fe3aaf86013effa7fb8fa3381ad1c
NorIslam04 <islamaoudi18@gmail.com>
2024-10-22 23:03:30 +0100

    add js staticFile to index.html (same problem css no linked !)

---
+++ project/static/js/script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function() {
+    alert("Le fichier JavaScript est correctement lié !");
+    console.log("Fichier JavaScript chargé avec succès !");
+});
