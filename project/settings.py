c51611bf5088c75fe17f23706476a21c0f293d30
NorIslam04 <islamaoudi18@gmail.com>
2024-10-25 17:09:06 +0100

    for pull

--- project/settings.py
+++ project/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': 'django_db',
         'USER': 'root',
         'PASSWORD': 'islam',
