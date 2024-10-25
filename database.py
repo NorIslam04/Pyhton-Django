4010b2d9cf954bbb2f1ff9648fa826dd1d0d9651
NorIslam04 <islamaoudi18@gmail.com>
2024-10-25 10:58:00 +0100

    connect to database mysql local + configuration in settings + file database.py

---
+++ database.py
@@ -0,0 +1,20 @@
+# Install Mysql on your computer
+# https://dev.mysql.com/downloads/installer/
+# pip install mysql
+# pip install mysql-connector
+# pip install mysql-connector-python 
+
+import mysql.connector
+
+dataBase = mysql.connector.connect(
+	host = 'localhost',
+	user = 'root',
+	passwd = 'islam'
+
+	)
+
+# prepare a cursor object
+cursorObject = dataBase.cursor()
+
+
+print("All Done!")
--- project/settings.py
+++ project/settings.py
@@ -79,7 +79,11 @@ WSGI_APPLICATION = 'project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'django_db',
+        'USER': 'root',
+        'PASSWORD': 'islam',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
